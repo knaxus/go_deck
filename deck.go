61674b3c3263f53a29db089d3db5b5df268824ae
Ashok Dey <ashokdey100@gmail.com>
2018-01-24 17:35:26 +0530

    --fix : for loop using _

--- deck.go
+++ deck.go
@@ -16,8 +16,8 @@ func newDeck() deck {
 	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for suit := range cardSuites {
-		for value := range cardValues {
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
