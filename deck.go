1c9914bd470df377bc939d497a003c462c5fe201
Ashok Dey <ashokdey100@gmail.com>
2018-01-24 17:27:07 +0530

    add: newDeck function in type deck

--- deck.go
+++ deck.go
@@ -9,8 +9,23 @@ import (
 
 type deck []string
 
-// print function to list all the cards in a deck
+// function to create all the 52 cards of a deck
+func newDeck() deck {
+	cards := deck{}
 
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for suit := range cardSuites {
+		for value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// print function to list all the cards in a deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
