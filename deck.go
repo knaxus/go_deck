a3f2834af98ce631f2ac6d6a7e842f8fe9d99de2
Ashok Dey <ashokdey100@gmail.com>
2018-01-24 17:39:50 +0530

    --update : complete deck with cards

--- deck.go
+++ deck.go
@@ -14,7 +14,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Joker"}
 
 	for _, suit := range cardSuites {
 		for _, value := range cardValues {
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamond", newCard()}
-	cards = append(cards, "Six of Spade")
-
+	cards := newDeck()
 	cards.print()
 }
 
