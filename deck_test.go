3e80afceb1a29e39c7e23da1fd2cd9a3ad3dc47d
Ashok Dey <ashokdey100@gmail.com>
2018-01-26 01:03:22 +0530

    --test : more test cases

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,14 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected the deck to have 52 cards but got: %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, got: %v", d[0])
+	}
+
+	lastCard := d[len(d)-1]
+
+	if lastCard != "Joker of Clubs" {
+		t.Errorf("Expected last card to be Joker of Clubs, got: %v", lastCard)
+	}
 }
